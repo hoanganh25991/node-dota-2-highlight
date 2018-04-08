6733818bba9fc503e7d444ba59b827d77d42a637
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-04-08 16:15:55 +0700

    Saved

--- src/autoClick/dota-2.js
+++ src/autoClick/dota-2.js
@@ -4,6 +4,9 @@ export default {
   watchBtn: { x: 787, y: 12 },
   replayBtn: { x: 928, y: 84 },
   searchBar: { x: 1567, y: 79 },
-  pressSearch: { x: 1815, y: 84 },
-  downloadReplay: { x: 949, y: 835 }
+  pressSearch: { x: 1814, y: 86 },
+  downloadReplay: { x: 949, y: 835 },
+  "": { x: 1463, y: 754 },
+  asdf: { x: 1698, y: 74 },
+  watchReplay: { x: 950, y: 845 }
 }
