11aa51eb336e8827bf9f68f54c3e3afc11a7aec4
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-04-08 12:02:27 +0700

    Update dota 2 step

--- src/autoClick/dota-2.js
+++ src/autoClick/dota-2.js
@@ -1,22 +1,9 @@
 export default {
-  playBtn: {
-    x: 400,
-    y: 200
-  },
-  playConfirmBtn: {
-    x: 997,
-    y: 602
-  },
-  watchBtn: {
-    x: 787,
-    y: 12
-  },
-  replayBtn: {
-    x: 928,
-    y: 84
-  },
-  searchBar: {
-    x: 1567,
-    y: 79
-  }
+  playBtn: { x: 400, y: 200 },
+  playConfirmBtn: { x: 997, y: 602 },
+  watchBtn: { x: 787, y: 12 },
+  replayBtn: { x: 928, y: 84 },
+  searchBar: { x: 1567, y: 79 },
+  pressSearch: { x: 1815, y: 84 },
+  downloadReplay: { x: 949, y: 835 }
 }
